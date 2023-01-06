ae2210e705207112043652906d2302f18929f601
mely-0 <3145756813mela@gmail.com>
2023-01-06 16:20:20 -0500

    firts commit

---
+++ src/components/Caroulsel.jsx
@@ -0,0 +1,57 @@
+import image3 from '../components/img/js2.png'
+import image from '../components/img/html2.png'
+import image2 from '../components/img/css2.png'
+import image6 from '../components/img/node-js.png'
+import image5 from '../components/img/php.png'
+import image4 from '../components/img/react2.png'
+function Carousel () {
+  return(
+      <div id="carouselExampleIndicators" className="carousel slide">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+  </div>
+  <div id='cont-habi' className="carousel-inner">
+    <div id='habi1' className="carousel-item active" >
+      <p>JAVA SCRIPT</p>
+      <img src={image3} className="d-block w-100" alt="..." id='js'/>
+    </div>
+    {/* <div id='habi2' className="carousel-item">
+      <p>PHP</p>
+      <img src={image5} className="d-block w-100" alt="..." id='css'/>
+    </div> */}
+    <div id='habi3' className="carousel-item">
+      <p>HTML</p>
+      <img src={image} className="d-block w-100" alt="..." id='html'/>
+    </div>
+
+    {/* <div id='habi4' className="carousel-item">
+      <p>REACT</p>
+      <img src={image4} className="d-block w-100" alt="..." id='html'/>
+      </div>
+      <div id='habi5' className="carousel-item">
+      <p>NODE JS</p>
+      <img src={image6} className="d-block w-100" alt="..." id='html'/>
+    </div> */}
+    <div id='habi6' className="carousel-item">
+      <p>CSS</p>
+      <img src={image2} className="d-block w-100" alt="..." id='html'/>
+    </div>
+  </div>
+  <button className="carousel-control-prev text-light" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true" id='click'></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true" id='click'></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+
+  )
+}
+
+export default Carousel
---
+++ src/components/Portafolio.jsx
@@ -0,0 +1,113 @@
+import React from 'react'
+import '../components/portafolio.css'
+import Caroulsel from '../components/Caroulsel'
+import { FaGithub } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { HiMail } from "react-icons/hi";
+export const Portafolio = () => {
+
+  return (
+    
+    <div className='portafolio'>
+      <nav id="navbar-example2" className="navbar bg-body-tertiary">
+  <ul className="nav nav-pills">
+    <li className="nav-item">
+      <a className="nav-link" href="#contenedor1">Portafolio</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link" href="#sobremi">Sobremi</a>
+    </li>
+    <li className="nav-item dropdown">
+      <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Menu</a>
+      <ul className="dropdown-menu">
+        <li><a className="dropdown-item" href="#habilidades">Habilidades</a></li>
+        <li><a className="dropdown-item" href="#container-pro">Proyectos</a></li>
+        <li><hr className="dropdown-divider"/></li>
+        <li><a className="dropdown-item" href="#contactos-pro">Contactos</a></li>
+      </ul>
+    </li>
+  </ul>
+</nav>
+    <div className='contenedor1' id='contenedor1'>
+      <section className='texto'>
+        <h1>Guillermo Morris</h1>
+        <p>Programador de software , 24 años.</p>
+      </section>
+      <section className='foto'></section>
+    </div>
+    <section className='picture'></section>
+
+    <div className='sobremi' id='sobremi'>
+
+      <h5 className='aboutme'>
+        Sobre mi 
+        <hr />
+        Soy una persona apasionada por aprender cosas nuevas, me considero un joven comprometido y responsable es mis obligaciones y con la mejor disposicion de crear nuevas habilidades.</h5>
+      <div className='habilidades' id='habilidades'>
+      {<Caroulsel/>}
+      </div>
+    </div>
+    <div className='container-pro' id='container-pro'>
+      <article className='tituloProyecto'>
+      <h3>Proyectos</h3>
+      </article>
+    <div className="container">
+        <div className="card">
+            <div className="face face1">
+                <div className="content">
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"/>
+                    <h3>Hoja de vida</h3>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                <a href="https://regal-marigold-6a8356.netlify.app/"> Ver</a>
+                </div>
+            </div>
+        </div>
+        <div className="card">
+            <div className="face face1">
+                <div className="content">
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true"/>
+                    <h3>Criptomonedas</h3>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                        <a href="https://curious-yeot-ee48bd.netlify.app/">Ver</a>
+                </div>
+            </div>
+        </div>
+        <div className="card">
+            <div className="face face1">
+                <div className="content">
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true"/>
+                    <h3>Proyecto Chakra ui </h3>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                  <a href="https://precious-yeot-0e927e.netlify.app/">ver</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div className='contactos-pro' id='contactos-pro'>
+    <div className='contactos'>
+        <a href="https://github.com/GuillermoMorris15" className='img-1'>
+      <article >
+      <FaGithub className='ico-1'/>
+      </article>
+      </a>
+      <article className='img-2'>
+        <FaFacebookSquare className='ico-2'/>
+      </article>
+      <article className='img-3'>
+        <HiMail className='ico-3'/>
+      </article>
+    </div>
+    </div>
+  </div>
+  )
+}
